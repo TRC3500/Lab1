372dbab72cf6d45cdd271d8c009ff9f00b2a364c
Cian Rodriguez <30370937+SpaceBarrr@users.noreply.github.com>
2024-02-17 22:36:38 +1100

    Create Print_On_UART.c

---
+++ Print_On_UART.c
@@ -0,0 +1,16 @@
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    UART_1_Start();
+    for(;;)
+    {
+        UART_1_WriteTxData(0x20);
+        CyDelay(500);
+        UART_1_PutString("Hello World");
+        CyDelay(500);
+    }
+}
